6dfdec678cbcd4fbca4d39d3b24869ccbaf8deb6
Live session user <chithrarm1996@gmail.com>
2017-09-16 11:44:08 +0000

    chatbot

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -19,7 +19,7 @@ app.controller('chatController', function ($scope,$sce) {
        }
    ];
 
-   var  exampleSocket =  new  WebSocket("ws://localhost:9000/chatSocket");
+   var  exampleSocket =  new  WebSocket("wss://swiftcode-chithra.herokuapp.com/chatSocket");
     exampleSocket.onmessage  =   function  (event)
      {
         var jsonData = JSON.parse(event.data);
